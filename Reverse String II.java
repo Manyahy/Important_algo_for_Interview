af7abf14cf53bfd7d1e61f1f6c6ca5342ced35a9
Manya H Y <hymanya5@gmail.com>
2025-09-09 22:37:30 +0530

    Create Reverse String II.java

---
+++ Reverse String II.java
@@ -0,0 +1,44 @@
+/*541. Reverse String II
+
+Given a string s and an integer k, reverse the first k characters for every 2k characters counting from the start of the string.
+
+If there are fewer than k characters left, reverse all of them. If there are less than 2k but greater than or equal to k characters, then reverse the first k characters and leave the other as original.
+
+ 
+
+Example 1:
+
+Input: s = "abcdefg", k = 2
+Output: "bacdfeg"
+Example 2:
+
+Input: s = "abcd", k = 2
+Output: "bacd"
+ 
+
+Constraints:
+
+1 <= s.length <= 104
+s consists of only lowercase English letters.
+1 <= k <= 104*/
+class Solution {
+    public String reverseStr(String s, int k) {
+        char[] arr = s.toCharArray();
+        int n = arr.length;
+
+        for (int start = 0; start < n; start += 2 * k) {
+            int i = start;
+            int j = Math.min(start + k - 1, n - 1); // reverse only first k chars or remaining
+
+            while (i < j) {
+                char temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+                i++;
+                j--;
+            }
+        }
+
+        return new String(arr);
+    }
+}
